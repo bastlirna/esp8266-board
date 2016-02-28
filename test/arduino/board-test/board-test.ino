c6bd096f10810584fdbece548abe3fb9922fe6ef
ah01 <horcicaa@gmail.com>
2016-02-28 01:31:52 +0100

    Test SW

---
+++ test/arduino/board-test/board-test.ino
@@ -0,0 +1,98 @@
+
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#include <pcf8574_esp.h>
+
+#include "hal.h"
+#include "tests.h"
+
+#ifdef ESP8266
+extern "C" {
+	#include "user_interface.h"
+}
+#endif
+
+void setup()
+{
+	Serial.begin(9600);
+	Serial.println();
+
+}
+
+bool i2c_mode;
+
+void loop()
+{
+	P("--- INFO -----------------------------------------------------------------------");
+
+	Serial.print("Serial ID: ");
+	Serial.println(system_get_chip_id());
+
+	Serial.print("Mac address: ");
+	Serial.println(WiFi.macAddress());
+
+	i2c_mode = is_i2c_available();
+
+	Serial.print("Board version: ");
+
+	if (i2c_mode)
+	{
+		Serial.println("FULL");
+	}
+	else
+	{
+		Serial.println("LITE");
+	}
+
+	P("");
+	P("--- TEST -----------------------------------------------------------------------");
+
+	error_counter = 0;
+	ok_counter = 0;
+
+	test_rgb();
+	test_beep();
+	test_dht();
+
+	if (i2c_mode)
+	{
+		//test_i2c();
+		test_i2c_btn();
+		test_i2c_relay();
+	}
+	else
+	{
+		test_btn();
+	}
+
+	P("--- RESULT ---------------------------------------------------------------------");
+	P("")
+
+	if (error_counter == 0)
+	{
+		Serial.print("PASS (");
+		Serial.print(ok_counter);
+		Serial.print(" tests ok)");
+		
+		digitalWrite(PIN_RGB_G, LOW);
+		delay(3000);
+		digitalWrite(PIN_RGB_G, HIGH);
+	}
+	else
+	{
+		Serial.print("FAIL (");
+		Serial.print(ok_counter);
+		Serial.print(" tests ok, ");
+		Serial.print(error_counter);
+		Serial.print(" tests fails)");
+
+		digitalWrite(PIN_RGB_R, LOW);
+		delay(3000);
+		digitalWrite(PIN_RGB_R, HIGH);
+	}
+
+	P("")
+	
+	delay(60000);
+}
+
---
+++ test/arduino/board-test/hal.h
@@ -0,0 +1,21 @@
+
+#define PIN_RGB_R 14
+#define PIN_RGB_G 12
+#define PIN_RGB_B 16
+
+#define PIN_BEEP 15
+
+#define PIN_BTN1 4
+#define PIN_BTN2 5
+
+#define PIN_DHT 13
+
+
+
+#define DHT_TYPE DHT11
+
+
+#define I2C_BTN1 4
+#define I2C_BTN2 5
+#define I2C_RELAY 7
+
